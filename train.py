7fd4804ca348b937e2bb4ba2364512c7616dd317
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-25 15:10:06 +0900

    First commits

---
+++ train.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import keras
+from keras.preprocessing.image import array_to_img, img_to_array, list_pictures, load_img
+
+
+if __name__ == "__main__":
+    
+    X = []
+    y = []
+
+    print('Load dataset')
+    for picture in list_pictures('./train/TC/', ext='tif'):
+        img = img_to_array(load_img(picture, target_size=(64, 64)))
+        X.append(img)
+        y.append(0)
+
+    print('Convert data')
+    X = np.asarray(X)
+    y = np.asarray(y)
+    X = X.astype('float32')
+    X /= 255.0
+
+    print(X.shape)
