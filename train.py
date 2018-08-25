44a86e0ef498e38d6e915e01c6235d9d5a21359b
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-25 15:41:21 +0900

    Update

--- train.py
+++ train.py
@@ -1,16 +1,18 @@
+import glob, os
 import numpy as np
 import pandas as pd
 import keras
 from keras.preprocessing.image import array_to_img, img_to_array, list_pictures, load_img
 
-
 if __name__ == "__main__":
     
     X = []
     y = []
+    
+    files = glob.glob('./train/TC/*.tif')
 
     print('Load dataset')
-    for picture in list_pictures('./train/TC/', ext='tif'):
+    for picture in files:
         img = img_to_array(load_img(picture, target_size=(64, 64)))
         X.append(img)
         y.append(0)
