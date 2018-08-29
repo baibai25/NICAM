d163947ea34fc4d21c1e0bfa92aedb350b3ff907
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-30 08:54:27 +0900

    Update

--- data.sh
+++ data.sh
@@ -4,7 +4,7 @@ for file in `ls zip/*zip`
 do
     echo $file
     unzip -q $file -d ./tmp/
-    find tmp/train/TC -type f | xargs -i mv "{}" train/TC
-    find tmp/train/nonTC -type f | xargs -i mv "{}" train/nonTC
+    find tmp/train/TC -type f | xargs -i mv "{}" data/train/TC
+    find tmp/train/nonTC -type f | xargs -i mv "{}" data/train/nonTC
     rm -r ./tmp/*
 done
