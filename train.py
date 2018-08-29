8444a022121de7c9a217d2eb7ab7725b58beca76
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-29 17:35:09 +0900

    Update train.py

--- train.py
+++ train.py
@@ -2,9 +2,14 @@ import glob, os, gc
 import numpy as np
 import pandas as pd
 import keras
-from keras.preprocessing.image import array_to_img, img_to_array, list_pictures, load_img
-from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array, load_img
 from keras.utils import np_utils
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, Flatten, Dense, Dropout, Activation 
+from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import Adamax
+from keras import backend as K
+from sklearn.model_selection import train_test_split
 
 # Preprocessing data
 def pre_data(X, y):
@@ -23,22 +28,17 @@ def build_model():
     # frames.shape = (frames width, hight, channels)
     model = Sequential()
     model.add(Conv2D(32, (3, 3), padding='same', data_format='channels_last', 
-                     activation=swish, input_shape=(64, 64, 3)))
-    model.add(Conv2D(32, (3, 3), activation=swish))
+                     activation=swish, input_shape=(64, 64, 1)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-
     model.add(Conv2D(64, (3, 3), padding='same', activation=swish))
-    model.add(Conv2D(64, (3, 3), activation=swish))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-
     model.add(Flatten())
     model.add(Dense(512, activation=swish))
     model.add(Dropout(0.5))
     model.add(Dense(2, activation='softmax'))
     return model
-
 
 if __name__ == "__main__":
     # Load data
@@ -49,12 +49,12 @@ if __name__ == "__main__":
 
     print('Load dataset')
     for picture in tc:
-        img = img_to_array(load_img(picture, target_size=(64, 64)))
+        img = img_to_array(load_img(picture, grayscale=True, target_size=(64, 64)))
         X.append(img)
         y.append(1)
 
     for picture in nontc:
-        img = img_to_array(load_img(picture, target_size=(64, 64)))
+        img = img_to_array(load_img(picture, grayscale=True, target_size=(64, 64)))
         X.append(img)
         y.append(0)
 
@@ -71,13 +71,12 @@ if __name__ == "__main__":
     print(X_train.shape)
     print(y_train.shape)
 
-"""
     # Learning
     model = build_model()
     model.compile(loss='binary_crossentropy', optimizer=Adamax(), metrics=['acc'])
     model.fit(X_train, y_train, epochs=10, batch_size=64, 
             validation_data=(X_val, y_val), verbose=1, shuffle=True)
-    
+        
     # export model
     model.save('my_model.h5')
-"""
+
