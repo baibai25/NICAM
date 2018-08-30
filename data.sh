670410d56a4a93bb754e033b05bc5d98f17cc104
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-30 09:07:18 +0900

    Update

--- data.sh
+++ data.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-for file in `ls zip/*zip`
+for file in `ls zip/train/*zip`
 do
     echo $file
     unzip -q $file -d ./tmp/
     find tmp/train/TC -type f | xargs -i mv "{}" data/train/TC
-    find tmp/train/nonTC -type f | xargs -i mv "{}" data/train/nonTC
+    find tmp/train/nonTC -type f | xargs -i mv "{}" data/train/nonTC  
+    #find tmp/test -type f | xargs -i mv "{}" data/test/img
     rm -r ./tmp/*
 done
