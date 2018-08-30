d499a638601b9c4510bee78fcad79376b577a02e
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-30 14:46:42 +0900

    Update

--- pred.py
+++ pred.py
@@ -2,30 +2,36 @@ import glob, os
 import pandas as pd
 import numpy as np
 import keras
-from keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 from keras import backend as K
+
+batch_size = 1
 
 # Activation Swish
 def swish(x):
     return x * K.sigmoid(x)
 
 if __name__ == "__main__":
-   # Load data
-    X_test = []
-    name = []
-    files = glob.glob('./test/*.tif')
-
-    for picture in files:
-        img = img_to_array(load_img(picture, grayscale=True, target_size=(64, 64)))
-        X_test.append(img)
-        name.append(os.path.basename(picture))
+    # Load data
+    test_datagen = ImageDataGenerator(rescale=1./255)
     
-    X_test = np.asarray(X_test)
-    X_test = X_test.astype('float32')
-    X_test /= 255.0
+    test_generator = test_datagen.flow_from_directory(
+        './data/test',
+        target_size=(64, 64),
+        color_mode='grayscale',
+        batch_size=batch_size,
+        shuffle=False,
+        seed=None,
+        class_mode=None
+    )
+
+    name = []
+ 
+    for i in range(len(path)):
+        name.append(os.path.basename(path[i]))
 
     # Testing and export result
     model = load_model('my_model.h5', custom_objects={'swish':swish})
-    y_prob = model.predict_proba(X_test)
+    y_prob = model.predict_generator(test_generator, len(test_generator.filenames), verbose=1)
     pd.DataFrame({'ID':name, 'prob':y_prob[:, 1]}).to_csv("submit.tsv", sep='\t', index=False, header=False)
