d103ad492f58a7da7ec0c9810aad8bab342d5a10
baibai25 <kamuy.camel.0225@gmail.com>
2018-08-31 21:06:04 +0900

    Update

--- augment.py
+++ augment.py
@@ -2,8 +2,10 @@ import glob
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 
+batch_size =128
+
 if __name__ == "__main__":
-    # Number of positive data 
+    # Number of positive data: 71,779 
     #files = glob.glob('./data/train/TC/origin/*tif')
     #print(len(files))
     
@@ -11,6 +13,7 @@ if __name__ == "__main__":
     gen = ImageDataGenerator(
         rescale=1./255,
         rotation_range=20,
+        zoom_range=0.2,
         width_shift_range=0.2,
         height_shift_range=0.2,
         horizontal_flip=True,
@@ -22,12 +25,12 @@ if __name__ == "__main__":
         target_size=(64, 64),
         color_mode='grayscale',
         batch_size=batch_size,
+        class_mode=None,
         save_to_dir='./data/train/TC/fake',
         save_format='tif'
     )
-   
+"""   
     # Generate fake images
-    for i in range(2):
+    for i in range(30):
         data_generator.next()
-
-
+"""
