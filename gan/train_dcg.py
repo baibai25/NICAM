0c02c7927ceae7bca4d2fc96b9ebc91c173f03cd
baibai25 <kamuy.camel.0225@gmail.com>
2018-09-08 21:32:28 +0900

    Update

--- gan/train_dcg.py
+++ gan/train_dcg.py
@@ -1,15 +1,30 @@
 import os
 import numpy as np
-from PIL import Image
-
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, BatchNormalization
-from keras.layers import Activation, Flatten, Dropout, Reshape, Conv2DTranspose
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Conv2D, Conv2DTranspose, BatchNormalization, Activation, Flatten, Reshape
 from keras.layers.advanced_activations import LeakyReLU
-
-batch_size = 64
+from keras.optimizers import Adam
 np.random.seed(42)
+
+def load_dataset(dataset_path):
+    
+    gen = ImageDataGenerator()
+    
+    data_generator = gen.flow_from_directory(
+        dataset_path,
+        target_size=(64, 64),
+        color_mode='grayscale',
+        batch_size=batch_size,
+        shuffle=False,
+        seed=None,
+        class_mode=None,
+    )
+
+    return data_generator
 
 # define model
 def generator_model():
@@ -38,6 +53,9 @@ def generator_model():
     generator.add(Conv2D(1, kernel_size=(4, 4), padding='same', strides=(1, 1)))
     generator.add(Activation('tanh'))
 
+    optimizer = Adam(lr=0.00015, beta_1=0.5)
+    generator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=None)
+
     return generator
 
 def discriminator_model():
@@ -65,38 +83,125 @@ def discriminator_model():
     discriminator.add(Dense(1))
     discriminator.add(Activation('sigmoid'))
     
+    optimizer = Adam(lr=0.0002, beta_1=0.5)
+    discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=None)
+
     return discriminator
 
-def gen_noise(batch_size, noise_shape):
-    return np.random.normal(0, 1, size=(batch_size,)+noise_shape)
+# Displays a figure of the generated images and saves them in as .png image
+def save_generated_images(generated_images, epoch, batch_number):
+    plt.figure(figsize=(8, 8), num=2)
+    gs1 = gridspec.GridSpec(8, 8)
+    gs1.update(wspace=0, hspace=0)
+
+    for i in range(64):
+        ax1 = plt.subplot(gs1[i])
+        ax1.set_aspect('equal')
+        image = generated_images[i, :, :, :]
+        image += 1
+        image *= 127.5
+        fig = plt.imshow(image.astype(np.uint8))
+        plt.axis('off')
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+
+    plt.tight_layout()
+    save_name = 'generated images/generatedSamples_epoch' + str(
+        epoch + 1) + '_batch' + str(batch_number + 1) + '.png'
+
+    plt.savefig(save_name, bbox_inches='tight', pad_inches=0)
+    plt.pause(0.0000000001)
+    plt.show()
 
 
-def combined_model(generator, discriminator):
-    model = Sequential()
-    model.add(generator)
-    model.add(discriminator)
-    return model
+def train(dataset_path, batch_size, epochs):
+    # Build network
+    generator = generator_model()
+    discriminator = discriminator_model()
+    discriminator.trainable = False
+    
+    gan = Sequential()
+    gan.add(generator)
+    gan.add(discriminator)
 
+    optimizer = Adam(lr=0.00015, beta_1=0.5)
+    gan.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=None)
+    #discriminator.summary()
+    #generator.summary()
+    #gan.summary()
+ 
+    # Load dataset
+    dataset_generator = load_dataset(dataset_path)
+    number_of_batches = int(71779 / batch_size)
+
+    # plot variables
+    adversarial_loss = np.empty(shape=1)
+    discriminator_loss = np.empty(shape=1)
+    batches = np.empty(shape=1)
+
+    plt.ion()
+    current_batch = 0
+
+    # Train
+    for epoch in range(epochs):
+        print("Epoch " + str(epoch+1) + "/" + str(epochs) + " :")
+
+        # Train discriminator
+        for batch_number in range(number_of_batches):
+            # Rescale images between -1 and 1
+            real_images = dataset_generator.next()
+            real_images /= 127.5
+            real_images -= 1
+
+            current_batch_size = real_images.shape[0]
+
+            # Generate noise and image
+            # noise.shape = (current_batch_size, 1, 1, 100)
+            noise = np.random.normal(0, 1, size=(current_batch_size, ) + (1, 1, 100))
+            generated_images = generator.predict(noise)
+
+            # Add noise to the labels
+            real_y = (np.ones(current_batch_size) - np.random.random_sample(current_batch_size) * 0.2)
+            fake_y = np.random.random_sample(current_batch_size) * 0.2
+
+    
+            # Train discriminator
+            # learn real_y and fake_y
+            discriminator.trainable = True
+            d_loss = discriminator.train_on_batch(real_images, real_y)
+            d_loss += discriminator.train_on_batch(generated_images, fake_y)
+            discriminator_loss = np.append(discriminator_loss, d_loss)
+
+
+            # Train generator
+            # try to mislead the discriminator by giving fake lavel
+            discriminator.trainable = False
+            noise = np.random.normal(0, 1, size=(current_batch_size * 2,) + (1, 1, 100))
+            fake_y = (np.ones(current_batch_size * 2) - np.random.random_sample(current_batch_size * 2) * 0.2)
+            g_loss = gan.train_on_batch(noise, fake_y)
+            adversarial_loss = np.append(adversarial_loss, g_loss)
+            batches = np.append(batches, current_batch)
+
+
+            # Each 50 batches show and save images
+            if((batch_number + 1) % 50 == 0 and current_batch_size == batch_size):
+                save_generated_images(generated_images, epoch, batch_number)
+            
+            current_batch += 1
+        
+        # Save the model weights each 5 epochs
+        if (epoch + 1) % 5 == 0:
+            discriminator.trainable = True
+            generator.save('models/generator_epoch' + str(epoch) + '.hdf5')
+            discriminator.save('models/discriminator_epoch' + str(epoch) + '.hdf5')
+
+
+def main():
+    dataset_path = ''
+    batch_size = 64
+    epochs = 190
+    train(dataset_path, batch_size, epochs)
 
 
 if __name__ == "__main__":
-    #noise = gen_noise(batch_size, noise_shape)
-    generator = generator_model()
-    generator.summary()
-
-    discriminator = discriminator_model()
-    discriminator.summary()
-"""
-discriminator.trainable = False
-combined = combined_model(generator, discriminator)
-combined.summary()
-
-
-opt = keras.optimizers.SGD(lr=0.0005, momentum=0.9, nesterov=True)
-
-discriminator.trainable = True
-discriminator.compile(loss='binary_crossentropy', optimizer=opt)
-
-discriminator.trainable = False
-combined.compile(loss='binary_crossentropy', optimizer=opt)
-"""
+    main()
