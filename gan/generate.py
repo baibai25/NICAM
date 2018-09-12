e4cb2c8df61b5adc1d85751c9e7e7127d3475749
baibai25 <kamuy.camel.0225@gmail.com>
2018-09-12 17:24:08 +0900

    Update

--- gan/generate.py
+++ gan/generate.py
@@ -8,7 +8,7 @@ np.random.seed(42)
 # Save generated images
 def save_generated_images(generated_images):
 
-    for i in range(64):
+    for i in range(len(generated_images)):
         image = generated_images[i, :, :, :]
         image += 1
         image *= 127.5
@@ -18,20 +18,22 @@ def save_generated_images(generated_images):
 
 
 # Training
-def test(model_path):
+def test(model_path, generate_size):
     # Load model
     model = load_model(model_path)
-
-    noise = np.random.normal(0, 1, size=(64, ) + (1, 1, 100))
+    
+    # Generate images
+    noise = np.random.normal(0, 1, size=(generate_size, ) + (1, 1, 100))
     generated_images = model.predict(noise)
     
-    # Output shape (64, 64, 64, 1) 
+    # Output shape (generate_size, 64, 64, 1) 
     save_generated_images(generated_images)
 
   
 def main():
     model_path = './models/generator_epoch199.hdf5'
-    test(model_path)
+    generate_size = 128
+    test(model_path, generate_size)
 
 
 if __name__ == "__main__":
